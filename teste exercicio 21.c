f4c641555869e80aa7420cd6d9fc1adb7e0245b0
Daniel-online <86497631+Daniel-online@users.noreply.github.com>
2022-07-14 15:04:39 -0300

    Update teste exercicio 21.c
    esqueci de escrever a ariavel com %2f

--- teste exercicio 21.c
+++ teste exercicio 21.c
@@ -3,21 +3,21 @@
 #include <stdlib.h>
 
 int main() {
-//declaraçao de variaveis
+//declaraÃ§ao de variaveis
 float Nhours, hours, salmin,Nextra, extra, saltotal;
 //entrada de dados
 printf("Coloque seu salario...:");
 scanf("%f", &salmin);
 printf("Quantas horas extras voce trabalhou?...:");
 scanf("%f", &Nextra);
-printf("Quantas hora normais você trabalhou?...:");
+printf("Quantas hora normais vocÃª trabalhou?...:");
 scanf("%f", &Nhours);
 //processamento
 hours= salmin/8; //calculando o valor da hora trabalhada
 extra= salmin/4; //calculando o valor da hora extra
 saltotal= Nhours*hours + Nextra*extra;
 //saida de dados
-printf("Salario a receber é %f ...:", saltotal);
+printf("Salario a receber Ã© %2f ...:", saltotal);
     return 0;
 }
 
