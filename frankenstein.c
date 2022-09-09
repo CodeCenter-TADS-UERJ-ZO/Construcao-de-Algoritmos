6892258818bc941611583052a86d884357de603a
Daniel-online <86497631+Daniel-online@users.noreply.github.com>
2022-09-09 10:22:30 -0300

    Update frankenstein.c

--- frankenstein.c
+++ frankenstein.c
@@ -1,27 +1,46 @@
-#include<stdio.h>
+// Online C compiler to run C program online
+#include <stdio.h>
 #include<stdlib.h>
+
+int main() {
 //declaração de variaveis
-double sexo;
-int idade;
-char [10] nome;
+char nome, classe;
+double sexo, idade;
 //apresentação do programa
-printf("----------------------------"\n);
-printf("-----------Start------------"\n);
-printf("----------------------------"\n\n);
-printf("Por favor, insira as informaçoes necessárias"\n);
-printf("de cadastro nos campos abaixo...:"\n\n);
+printf("-----------------------------------\n");
+printf("---------------Start---------------\n");
+printf("-----------------------------------\n");
+printf("Por favor, insira as informaçoes   \n");
+printf("de cadastro nos campos abaixo...:\n\n");
 //area de cadastro de informações
-printf("==================================="\n);
+
+printf("===================================\n");
 printf("NOME...:");
-scanf("%c", &nome);
-printf("==================================="\n\n);
-//
-printf("==================================="\n);
-printf("Idade...:");
-scanf("%i", &idade);
-printf("==================================="\n\n);
-//
-printf("==================================="\n);
-printf("Sexo...:")
-scanf("%d", &sexo)
-printf("==================================="\n);
+gets("%c", &nome);
+
+printf("===================================\n");
+
+printf("===================================\n");
+printf("IDADE...:\n");
+gets(" %d", &idade);
+
+printf("===================================\n");
+
+printf("===================================\n");
+printf("SEXO...:\n");
+gets(" %d", &sexo);
+
+printf("===================================\n");
+
+printf("===================================\n");
+
+printf("===================================\n");
+printf("CLASSE...:\n");
+gets(" %c", &classe);
+
+printf("===================================\n");
+
+
+
+    return 0;
+}
